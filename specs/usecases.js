0396afb9cd98a3612ac56ba8a3cd43052c20af08
neochrome <johan@stenqvist.net>
2014-09-29 08:52:28 +0200

    removed last trace of working title

--- specs/usecases.js
+++ specs/usecases.js
@@ -1,8 +1,8 @@
 describe('Use cases', function () {
 
 	it('resolves module instance with requirements from specified load path', function () {
-		var jin = require('../src');
-		var kernel = jin.create('./modules');
+		var autoquire = require('../src');
+		var kernel = autoquire.create('./modules');
 
 		var instance = kernel.get('module-with-requirements');
 
@@ -10,8 +10,8 @@ describe('Use cases', function () {
 	});
 
 	it('resolves module instance with requirements from qualified module path', function () {
-		var jin = require('../src');
-		var kernel = jin.create();
+		var autoquire = require('../src');
+		var kernel = autoquire.create();
 
 		var instance = kernel.get('./modules/module-with-requirements');
 
